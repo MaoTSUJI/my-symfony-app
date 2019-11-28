576372f6d9c0e528a3b14b2e0ec4596a76fec9dd
辻真緒 <>
2019-11-28 16:42:17 +0900

    AbstractControllerの継承　getSubscribedServicesの利用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,25 +1,27 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     /**
      * @Route("/hello", name="hello")
      */
     public function index()
     {
-        $result = <<< EOM
-        <html>
-        <head><title>Hello</title></head>
-        <body>
-        <h1>Hello Symfony!</h1>
-        <p>this is Symfony sample page.</p>
-        </body>
-        </html>
-EOM;
+        $result = '<html><body>';
+        $result .= '<h1>Subscribed Services</h1>';
+        $result .= '<ol>';
+        $arr = $this->getSubscribedServices();
+
+        foreach ($arr as $key => $value) {
+            $result .= '<li>' .$key .'</li>';
+        }
+        $result .= '</ol>';
+        $result .= '</body></html>';
 
         return new Response($result);
     }
