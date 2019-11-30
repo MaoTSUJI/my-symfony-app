2a2885a2380899b5a6e7354d4fd63bb3b26d26fd
辻真緒 <>
2019-11-30 13:53:25 +0900

    3-1 twigの表示

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,11 +1,8 @@
 <?php
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
@@ -13,17 +10,9 @@ class HelloController extends AbstractController
     /**
      * @Route("/hello", name="hello")
      */
-    public function index(Request $request, LoggerInterface $logger)
+    public function index()
     {
-        $data = array(
-            'name'=>array('first'=>'Taro', 'second'=>'Yamada'),
-            'age'=>36, 'mail'=>'taro@yamada.kun'
-        );
-
-        $logger->info(serialize($data));
-
-        return new JsonResponse($data);
-
+        return $this->render('hello/index.html.twig',['controller_name' => 'HelloController']);
     }
 
 
