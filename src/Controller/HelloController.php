bef8068ae49b7730f0577f94f9f869e8096a7fe4
辻真緒 <>
2019-12-01 14:37:12 +0900

    formテンプレートの使用

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class HelloController extends AbstractController
 {
     /**
@@ -13,23 +16,23 @@ class HelloController extends AbstractController
      */
     public function index(Request $request)
     {
-        return $this->render('hello/index.html.twig', [
-            'title' => 'Hello',
-            'message' => 'あなたのお名前'
-        ]);
-    }
+        $form = $this->createFormBuilder()
+            ->add('input', TextType::class)
+            ->add('save', SubmitType::class, ['label' =>'Click'])
+            ->getForm();
 
-    /**
-     * @Route("/other", name="other")
-     */
-    public function other(Request $request)
-    {
-        $input = $request->request->get('input');
-        $msg = 'こんにちは'. $input . 'さん！';
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            $msg = 'こんにちは、' . $form->get('input')->getData() . 'さん！';
+        } else {
+            $msg = 'お名前をどうぞ！';
+        }
         return $this->render('hello/index.html.twig', [
-            'title' => 'Hello',
+            'title' =>'Hello',
             'message' => $msg,
+            'form' => $form->createView(),
         ]);
+
     }
 
 
